9414f5e82dfc77520a8563ab428d8b38c800079b
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-02-09 22:58:38 -0200

    configuring file-loader, sass and css

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import "./sass/main.scss";
+import icon from "./img/logo.png";
+
+document.querySelector("#icon").innerHTML = `<img src="${icon}" />`;
+
 console.log("------------------------------------");
-console.log("Hello webpack!!");
+console.log("Hello webpack!!    ");
 console.log("------------------------------------");
--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,31 @@ module.exports = {
     filename: "bundle.js",
     path: __dirname + "/dist"
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: [["es2015", { modules: false }]]
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "img/"
+          }
+        }
+      }
+    ]
+  },
   plugins: [HWPConfig]
 };
